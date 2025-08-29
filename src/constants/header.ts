b1b58e51a28bad5ddd9ca8d21c5c1a04629ac9a4
VincentiusElbert <vincentiuselbert@gmail.com>
2025-08-29 16:34:08 +0700

    Delete src directory

--- src/constants/header.ts
+++
@@ -1,8 +0,0 @@
-import type { NavItems } from "../types/header";
-
-export const PRIMARY_NAV_ITEMS: NavItems[] = [
-  { label: "Why Choose Us", path: "/why-choose-us" },
-  { label: "Module", path: "/module" },
-  { label: "Testimony", path: "/testimony" },
-  { label: "FAQ", path: "/faq" },
-];
--- src/types/header.ts
+++
@@ -1,8 +0,0 @@
-export interface NavItems {
-  label: string;
-  path: string;
-}
-
-export interface HeaderProps {
-  navItems: NavItems[];
-}
