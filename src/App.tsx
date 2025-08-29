1f7f8b87424184f7fc661a64aaf742be1c867d0b
VincentiusElbert <vincentiuselbert@gmail.com>
2025-08-29 16:54:58 +0700

    fix: remove unused React import

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 // src/App.tsx
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./layouts/header/Header"; // PATH DIUBAH DI SINI!
-import HomePage from "./pages/HomePage";
-import { SearchProvider } from "./context/SearchContext";
-import ProductDetailPage from "./pages/ProductDetailPage";
-import { CategoryFilterProvider } from "./context/CategoryFilterContext";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './layouts/header/Header'; // PATH DIUBAH DI SINI!
+import HomePage from './pages/HomePage';
+import { SearchProvider } from './context/SearchContext';
+import ProductDetailPage from './pages/ProductDetailPage';
+import { CategoryFilterProvider } from './context/CategoryFilterContext';
 // import ProductsPage from "./pages/ProductsPage";
 // import AboutPage from "./pages/AboutPage";
 // import ContactPage from "./pages/ContactPage";
-import CategoryProductsPage from "./pages/CategoryProductsPage";
-import { CartProvider } from "./context/CartContext";
-import CartPage from "./pages/CartPage";
+import CategoryProductsPage from './pages/CategoryProductsPage';
+import { CartProvider } from './context/CartContext';
+import CartPage from './pages/CartPage';
 
 function App() {
   return (
@@ -20,12 +19,18 @@ function App() {
         <CategoryFilterProvider>
           <CartProvider>
             <Header />
-            <main className="container mx-auto p-4 mt-4">
+            <main className='container mx-auto p-4 mt-4'>
               <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/products/:productId" element={<ProductDetailPage />} />
-                <Route path="/products/category/:categoryName" element={<CategoryProductsPage />} />
-                <Route path="/Cart" element={<CartPage />} />
+                <Route path='/' element={<HomePage />} />
+                <Route
+                  path='/products/:productId'
+                  element={<ProductDetailPage />}
+                />
+                <Route
+                  path='/products/category/:categoryName'
+                  element={<CategoryProductsPage />}
+                />
+                <Route path='/Cart' element={<CartPage />} />
               </Routes>
             </main>
           </CartProvider>
